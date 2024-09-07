4846e57f7a9eae08038965fbb3cae0c756bc2448
Jonas Miqueias <jhonasbola2@gmail.com>
2024-09-07 12:26:12 -0300

    criando a base de dados com JS

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    /*objetos, listas*/
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um dos maiores surfistas do mundo e pioneiro do surf brasileiro. Ele conquistou o título mundial de surf por várias vezes e continua a inspirar novas gerações no esporte.",
+        link: "https://www.instagram.com/gabrielmedina/?hl=pt-br"
+    },
+    {
+        titulo: "Alisson Becker",
+        descricao: "Alisson Becker é considerado um dos melhores goleiros do mundo. Ele tem sido uma peça fundamental para a Seleção Brasileira e para o Liverpool, com sua habilidade e liderança em campo.",
+        link: "https://www.instagram.com/alissonbecker/?hl=pt-br"
+    }
+];
