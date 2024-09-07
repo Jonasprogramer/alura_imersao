8f96be12eb817148127257ae7c48b0228e2a94c1
Jonas Miqueias <jhonasbola2@gmail.com>
2024-09-07 15:50:05 -0300

    finalizando o projeto com github e deploy

--- app.js
+++ app.js
@@ -2,22 +2,49 @@
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa")
 
-    let resultados = ""
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    //console.log(campoPesquisa)
+
+    //se campoPesquisa form uma string sem nada
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de uma atleta ou esporte</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = ""
 
     //para cada
     for(let dado of dados){
-        resultados += `
-        <div class="item-resultado">
-                        <h2>
-                            <a href="https://www.youtube.com/watch?v=U6QlKjfTSpc" target="_blank">${dado.titulo}</a>
-                        </h2>
-                        <p class="descricao-meta">
-                            ${dado.descricao}
-                        </p>
-                        <a href=${dado.link} target="_blank">Instagram</a>
-                    </div>` 
-    }
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        //se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
 
+            //cria um novo elemento HTML para cada Resultado
+            resultados += `
+            <div class="item-resultado">
+                            <h2>
+                                <a href="https://www.youtube.com/watch?v=U6QlKjfTSpc" target="_blank">${dado.titulo}</a>
+                            </h2>
+                            <p class="descricao-meta">
+                                ${dado.descricao}
+                            </p>
+                            <a href=${dado.link} target="_blank">Instagram</a>
+                        </div>` 
+            }
+
+        }
+
+        if(!resultados){
+            resultados = "<p>Nada foi encontrado</p>"
+        }
+        
     section.innerHTML = resultados
 }
 
--- dados.js
+++ dados.js
@@ -3,16 +3,19 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate jovem fadinha"
     },
     {
         titulo: "Gabriel Medina",
         descricao: "Gabriel Medina é um dos maiores surfistas do mundo e pioneiro do surf brasileiro. Ele conquistou o título mundial de surf por várias vezes e continua a inspirar novas gerações no esporte.",
-        link: "https://www.instagram.com/gabrielmedina/?hl=pt-br"
+        link: "https://www.instagram.com/gabrielmedina/?hl=pt-br",
+        tags: "surf mundial geracao"
     },
     {
         titulo: "Alisson Becker",
         descricao: "Alisson Becker é considerado um dos melhores goleiros do mundo. Ele tem sido uma peça fundamental para a Seleção Brasileira e para o Liverpool, com sua habilidade e liderança em campo.",
-        link: "https://www.instagram.com/alissonbecker/?hl=pt-br"
+        link: "https://www.instagram.com/alissonbecker/?hl=pt-br",
+        tags: "goleiro seleção liverpool"
     }
 ];
