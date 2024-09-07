e8a8837bf95b6f3d29be6fd97fb55afeecb37841
Jonas Miqueias <jhonasbola2@gmail.com>
2024-09-07 13:06:48 -0300

    implementando a funcionalidade de busca

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log(dados[1].titulo)
+//console.log(dados[1].titulo)
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+
+    let resultados = ""
+
+    //para cada
+    for(let dado of dados){
+        resultados += `
+        <div class="item-resultado">
+                        <h2>
+                            <a href="https://www.youtube.com/watch?v=U6QlKjfTSpc" target="_blank">${dado.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">
+                            ${dado.descricao}
+                        </p>
+                        <a href=${dado.link} target="_blank">Instagram</a>
+                    </div>` 
+    }
+
+    section.innerHTML = resultados
+}
+
